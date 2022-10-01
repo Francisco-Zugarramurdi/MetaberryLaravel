c123fda4480129158a464fe2136465193de2c5d9
sofiamotta <sofiamottasanchez03@gmail.com>
2022-10-01 20:29:58 -0300

    Backoffice + authAPI - changed method herarchy

--- backofficeApp/app/Http/Controllers/AdTagController.php
+++ backofficeApp/app/Http/Controllers/AdTagController.php
@@ -7,7 +7,7 @@ use \Illuminate\Database\QueryException;
 use App\Models\AdTag;
 class AdTagController extends Controller
 {
-    //
+
     public function create(Request $request){
         
         $validation = $this->validateRequest($request);
@@ -25,70 +25,6 @@ class AdTagController extends Controller
                 "trace" => $e -> getMessage()
             ];
         }
-    }
-    public function update(Request $request, $id){
-
-        $validation = $this->validateRequest($request);
-
-        if($validation !== "ok")
-            return $validation;
-        
-        try{
-            $this->updateAdTag($request, $id);
-            return "ok";
-        }
-        catch (QueryException $e){
-            return [
-                "error" => 'Cannot update Tag',
-                "trace" => $e -> getMessage()
-            ];
-        }
-    }
-
-    public function destroy($id){
-        try{
-            $tag = AdTag::findOrFail($id);
-            $tag->delete();
-            return "Tag destroyed";
-        }
-        catch(QueryException $e){
-            return [
-                "error" => 'Cannot delete Tag',
-                "trace" => $e -> getMessage()
-            ];
-        }
-    }
-
-    public function index(){
-        return AdTag::all();
-    }
-
-    private function updateAdTag($request, $id){
-        $tag = AdTag::findOrFail($id);
-
-        $tag -> id_ad = $request -> id_ad;
-        $tag -> tag = $request -> tag;
-
-        $tag -> save();
-    }
-
-    private function validateCreationRequest(Request $request){
-
-        $validator = Validator::make($request->all(),[
-
-            'ad_id' => 'required',
-            'tag' => 'required'
-            
-        ]);
-
-        if($validator->fails())
-            return $validator->errors()->toJson();
-
-        if(AdTag::withTrashed()->where('tag', $request -> post("tag")) -> exists())
-            return 'Tag already exists';
-
-        return 'ok';
-
     }
 
     private function validateRequest(Request $request){
@@ -114,5 +50,71 @@ class AdTagController extends Controller
 
         ]);
     }
+
+    private function validateCreationRequest(Request $request){
+
+        $validator = Validator::make($request->all(),[
+
+            'ad_id' => 'required',
+            'tag' => 'required'
+            
+        ]);
+
+        if($validator->fails())
+            return $validator->errors()->toJson();
+
+        if(AdTag::withTrashed()->where('tag', $request -> post("tag")) -> exists())
+            return 'Tag already exists';
+
+        return 'ok';
+
+    }
+
+    public function update(Request $request, $id){
+
+        $validation = $this->validateRequest($request);
+
+        if($validation !== "ok")
+            return $validation;
+        
+        try{
+            $this->updateAdTag($request, $id);
+            return "ok";
+        }
+        catch (QueryException $e){
+            return [
+                "error" => 'Cannot update Tag',
+                "trace" => $e -> getMessage()
+            ];
+        }
+    }
+
+    private function updateAdTag($request, $id){
+        $tag = AdTag::findOrFail($id);
+
+        $tag -> id_ad = $request -> id_ad;
+        $tag -> tag = $request -> tag;
+
+        $tag -> save();
+    }
+
+    public function destroy($id){
+        try{
+            $tag = AdTag::findOrFail($id);
+            $tag->delete();
+            return "Tag destroyed";
+        }
+        catch(QueryException $e){
+            return [
+                "error" => 'Cannot delete Tag',
+                "trace" => $e -> getMessage()
+            ];
+        }
+    }
+
+    public function index(){
+        return AdTag::all();
+    }
+
 }
 
