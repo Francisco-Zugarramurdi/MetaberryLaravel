78bf4db4f12674f0a81f808f96695cc026837ef1
LucasMedina16 <48605639+LucasMedina16@users.noreply.github.com>
2022-10-02 18:51:45 -0300

    Update metaberry.sql

--- metaberry.sql
+++ metaberry.sql
@@ -52,9 +52,9 @@ create table players_teams (
 );
  
 create table terminated_contracts(
-    id_players bigint unsigned,
-    id_teams bigint unsigned,
-    contract_end date,
+    id_players bigint unsigned not null,
+    id_teams bigint unsigned not null,
+    contract_end date not null,
     primary key (id_players,id_teams),
     foreign key (id_players, id_teams) references players_teams(id_players, id_teams)
 );
@@ -63,23 +63,23 @@ create table extras(
     id serial primary key,
     name varchar(50) not null,
     surname varchar(50) not null,
-    photo text,
-    rol varchar(50)
+    photo text not null,
+    rol varchar(50) not null
 );
  
 create table extra_compose(
-    id_extra bigint unsigned,
-    id_teams bigint unsigned,
+    id_extra bigint unsigned not null,
+    id_teams bigint unsigned not null,
     contract_start date not null,
-    contract_end date,
+    contract_end date not null,
     primary key (id_extra,id_teams),
     foreign key (id_extra) references extras(id),
     foreign key (id_teams) references teams(id)
 );
  
 create table events_teams(
-    id_teams bigint unsigned,
-    id_events bigint unsigned,
+    id_teams bigint unsigned not null,
+    id_events bigint unsigned not null,
     primary key (id_teams,id_events),
     foreign key (id_teams) references teams(id),
     foreign key (id_events) references events(id)
@@ -89,20 +89,20 @@ create table leagues(
     id serial primary key,
     name varchar(50) not null,
     details text not null,
-    photo text
+    photo text not null
 );
  
 create table leagues_events(
-    id_events bigint unsigned,
-    id_leagues bigint unsigned,
+    id_events bigint unsigned not null,
+    id_leagues bigint unsigned not null,
     primary key (id_events,id_leagues),
     foreign key (id_events) references events(id),
     foreign key (id_leagues) references leagues(id)
 );
  
 create table leagues_countries(
-    id_countries bigint unsigned,
-    id_leagues bigint unsigned,
+    id_countries bigint unsigned not null,
+    id_leagues bigint unsigned not null,
     primary key (id_countries,id_leagues),
     foreign key (id_countries) references countries(id),
     foreign key (id_leagues) references leagues(id)
@@ -110,15 +110,15 @@ create table leagues_countries(
  
 create table sanctions(
     id serial,
-    id_events bigint unsigned,
+    id_events bigint unsigned not null,
     sancion varchar(50) not null,
     primary key (id,id_events),
     foreign key (id_events) references events(id)
 );
  
 create table sanctions_players(
-    id_sancion bigint unsigned,
-    id_players bigint unsigned,
+    id_sancion bigint unsigned not null,
+    id_players bigint unsigned not null,
     dates date not null,
     primary key (id_sancion,id_players),
     foreign key (id_sancion) references sanctions(id),
@@ -126,8 +126,8 @@ create table sanctions_players(
 );
  
 create table sanctions_extra(
-    id_sancion bigint unsigned,
-    id_extra bigint unsigned,
+    id_sancion bigint unsigned not null,
+    id_extra bigint unsigned not null,
     dates date not null,
     primary key (id_sancion,id_extra),
     foreign key (id_sancion) references sanctions(id),
@@ -142,8 +142,8 @@ create table referee(
 );
  
 create table referee_events(
-    id_referee bigint unsigned,
-    id_events bigint unsigned,
+    id_referee bigint unsigned not null,
+    id_events bigint unsigned not null,
     dates date not null,
     primary key (id_referee,id_events),
     foreign key (id_events) references events(id),
@@ -153,11 +153,11 @@ create table referee_events(
 create table users_data(
     id serial primary key,
     name varchar(255) not null,
-    credit_card varchar(16),
-    photo text,
+    credit_card varchar(16) not null,
+    photo text not null,
     points bigint not null,
-    type_of_user varchar(6),
-    total_points bigint unsigned
+    type_of_user varchar(6) not null,
+    total_points bigint unsigned not null
 );
  
 create table users(
@@ -175,17 +175,17 @@ create table users(
 create table post(
     id serial,
     post text not null,
-    id_users_data bigint unsigned,
+    id_users_data bigint unsigned not null,
     dates date not null,
-    number_of_likes bigint,
-    id_events bigint unsigned,
+    number_of_likes bigint not null,
+    id_events bigint unsigned not null,
     primary key (id, id_users_data,id_events),
     foreign key (id_users_data) references users_data(id),
     foreign key (id_events) references events(id)
 );
 create table user_likes(
-    id_post bigint unsigned,
-    id_users_data bigint unsigned,
+    id_post bigint unsigned not null,
+    id_users_data bigint unsigned not null,
     primary key (id_post, id_users_data),
     foreign key (id_post) references post(id),
     foreign key (id_users_data) references users_data(id)
@@ -193,19 +193,19 @@ create table user_likes(
  
 create table comments (
     id serial,
-    comments text,
-    id_post bigint unsigned,
-    id_users_data bigint unsigned,
-    dates date,
-    is_child tinyint(1),
+    comments text not null,
+    id_post bigint unsigned not null,
+    id_users_data bigint unsigned not null,
+    dates date not null,
+    is_child tinyint(1) not null,
     primary key (id,id_post,id_users_data),
     foreign key (id_post) references post(id),
     foreign key (id_users_data) references users_data(id)
 );
  
 create table comments_parent(
-    id_comments_parent bigint unsigned,
-    id_comments_child bigint unsigned,
+    id_comments_parent bigint unsigned not null,
+    id_comments_child bigint unsigned not null,
     primary key (id_comments_parent,id_comments_child),
     foreign key (id_comments_parent) references comments(id),
     foreign key (id_comments_child) references comments(id)
@@ -213,48 +213,48 @@ create table comments_parent(
  
 create table reward(
     id serial,
-    reward varchar(50),
-    id_users_data bigint unsigned,
-    date_of_delivery date,
+    reward varchar(50) not null,
+    id_users_data bigint unsigned not null,
+    date_of_delivery date not null,
     primary key (id,id_users_data),
     foreign key (id_users_data) references users_data(id)
 );
  
 create table premium_league(
-    id_users_data bigint unsigned,
-    id_leagues bigint unsigned,
+    id_users_data bigint unsigned not null,
+    id_leagues bigint unsigned not null,
     primary key (id_users_data,id_leagues),
     foreign key (id_users_data) references users_data(id),
     foreign key (id_leagues) references leagues(id)
 );
  
 create table premium_sports(
-    id_users_data bigint unsigned,
-    id_sports bigint unsigned,
+    id_users_data bigint unsigned not null,
+    id_sports bigint unsigned not null,
     primary key (id_users_data,id_sports),
     foreign key (id_users_data) references users_data(id),
     foreign key (id_sports) references sports(id)
 );
  
 create table premium_events(
-    id_users_data bigint unsigned,
-    id_events bigint unsigned,
+    id_users_data bigint unsigned not null,
+    id_events bigint unsigned not null,
     primary key (id_users_data,id_events),
     foreign key (id_users_data) references users_data(id),
     foreign key (id_events) references events(id)
 );
  
 create table premium_teams(
-    id_users_data bigint unsigned,
-    id_teams bigint unsigned,
+    id_users_data bigint unsigned not null,
+    id_teams bigint unsigned not null,
     primary key (id_users_data,id_teams),
     foreign key (id_users_data) references users_data(id),
     foreign key (id_teams) references teams(id)
 );
  
 create table favourite_user(
-    id_users_data bigint unsigned,
-    id_teams bigint unsigned,
+    id_users_data bigint unsigned not null,
+    id_teams bigint unsigned not null,
     primary key (id_users_data,id_teams),
     foreign key (id_users_data) references users_data(id),
     foreign key (id_teams) references teams(id)
@@ -262,20 +262,20 @@ create table favourite_user(
  
 create table results(
     id serial,
-    tipo_results varchar(11),
-    results varchar(25),
-    id_events bigint unsigned,
+    tipo_results varchar(11) not null,
+    results varchar(25) not null,
+    id_events bigint unsigned not null,
     primary key (id,id_events),
     foreign key (id_events) references events(id)
 );
  
 create table bets(
     id serial,
-    amount int unsigned,
+    amount int unsigned not null,
     id_users_data bigint unsigned not null,
-    id_events bigint unsigned,
-    id_teams bigint unsigned,
-    dates date,
+    id_events bigint unsigned not null,
+    id_teams bigint unsigned not null,
+    dates date not null,
     primary key (id,id_users_data,id_events,id_teams),
     foreign key (id_users_data) references users_data(id),
     foreign key (id_teams) references teams(id),
@@ -283,20 +283,20 @@ create table bets(
 );
  
 create table points_sets(
-    number_set tinyint,
-    points_set int,
-    id_teams bigint unsigned,
-    id_results bigint unsigned,
+    number_set tinyint not null,
+    points_set int not null,
+    id_teams bigint unsigned not null,
+    id_results bigint unsigned not null,
     primary key (id_teams,id_results,number_set),
     foreign key (id_teams) references teams(id),
     foreign key (id_results) references results(id)
 );
  
 create table results_points(
-    id_results bigint unsigned,
-    id_teams bigint unsigned,
-    point int,
-    id_players bigint unsigned,
+    id_results bigint unsigned not null,
+    id_teams bigint unsigned not null,
+    point int not null,
+    id_players bigint unsigned not null,
     primary key (id_results,id_teams,id_players),
     foreign key (id_results) references results(id),
     foreign key (id_teams) references teams(id),
@@ -304,20 +304,20 @@ create table results_points(
 );
  
 create table results_upward(
-    id_results bigint unsigned,
-    id_teams bigint unsigned,
-    position  int,
-    result time,
+    id_results bigint unsigned not null,
+    id_teams bigint unsigned not null,
+    position int not null,
+    result time not null,
     primary key (id_results,id_teams),
     foreign key (id_results) references results(id),
     foreign key (id_teams) references teams(id)
 );
  
 create table results_downward(
-    id_results bigint unsigned,
-    id_teams bigint unsigned,
-    position  int,
-    result time,
+    id_results bigint unsigned not null,
+    id_teams bigint unsigned not null,
+    position int not null,
+    result time not null,
     primary key (id_results,id_teams),
     foreign key (id_results) references results(id),
     foreign key (id_teams) references teams(id)
@@ -325,11 +325,11 @@ create table results_downward(
  
 create table ads(
     id serial,
-    view_counter bigint,
-    views_hired bigint,
-    url text,
-    image text,
-    size varchar(10),
+    view_counter bigint not null,
+    views_hired bigint not null,
+    url text not null,
+    image text not null,
+    size varchar(10) not null,
     created_at timestamp DEFAULT CURRENT_TIMESTAMP,
     updated_at timestamp DEFAULT CURRENT_TIMESTAMP,
     primary key (id)
@@ -337,15 +337,15 @@ create table ads(
 
 create table tags(
     id serial,
-    tag text,
+    tag text not null,
     created_at timestamp DEFAULT CURRENT_TIMESTAMP,
     updated_at timestamp DEFAULT CURRENT_TIMESTAMP,
     primary key (id)
 );
 
 create table ad_tags(
-    id_tag bigint unsigned,
-    id_ad bigint unsigned,
+    id_tag bigint unsigned not null,
+    id_ad bigint unsigned not null,
     primary key (id_tag, id_ad),
     foreign key (id_ad) references ads(id),
     foreign key (id_tag) references tags(id)
