03b612676ecc8575ff0a718b7dc4bc76d54a42a5
sofiamotta <sofiamottasanchez03@gmail.com>
2022-10-04 20:37:07 -0300

    Sport and Country Model

--- backofficeApp/app/Http/Controllers/SportController.php
+++ backofficeApp/app/Http/Controllers/SportController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Sport;
+use App\Models\Country;
 
 class SportController extends Controller
 {
-    //
+    
 }
--- backofficeApp/database/migrations/2022_10_04_232703_create_contries_table.php
+++ backofficeApp/database/migrations/2022_10_04_232703_create_contries_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSportsTable extends Migration
+class CreateContriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,9 @@ class CreateSportsTable extends Migration
      */
     public function up()
     {
-        Schema::create('sports', function (Blueprint $table) {
+        Schema::create('contries', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->string('name');
-            $table->string('photo');
         });
     }
 
@@ -28,6 +26,6 @@ class CreateSportsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('sports');
+        Schema::dropIfExists('contries');
     }
 }
