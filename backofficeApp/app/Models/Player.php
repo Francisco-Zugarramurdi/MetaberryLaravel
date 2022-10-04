b5cbf63196fefd162dfde6c58e2c8f9e7b5c5047
Francisco-Zugarramurdi <franciscozugarramurdi@gmail.com>
2022-10-04 20:41:53 -0300

    Ongoing work

--- backofficeApp/app/Models/Player.php
+++ backofficeApp/app/Models/Player.php
@@ -4,9 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
 class Player extends Model
 {
     use HasFactory,SoftDeletes;
     protected $table = "players";
+    protected $fillable = [
+        'name',
+        'surname',
+        'photo',
+    ];
 }
