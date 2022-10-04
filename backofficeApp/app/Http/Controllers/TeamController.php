811f4f2ea6033faec196a4890682098d32b11bf6
nicolasMoraGnd <89613192+nicolasMoraGnd@users.noreply.github.com>
2022-10-04 19:43:48 -0300

    team migration

--- backofficeApp/app/Http/Controllers/TeamController.php
+++ backofficeApp/app/Http/Controllers/TeamController.php
@@ -3,8 +3,54 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Team;
+use Illuminate\Support\Facades\Validator;
 
 class TeamController extends Controller
 {
-    //
+    public function create(Request $request){
+        $this->validateCreationRequest();
+        if($validation !== "ok"){
+            return $validation;
+        }
+        try {
+            return $this->createTeam($request);
+        }
+        catch (QueryException $e){
+
+            return [
+                "error" => 'Cannot create user',
+                "trace" => $e -> getMessage()
+            ];
+
+        }
+    }
+
+    private function validateCreationRequest(){
+        $validator = Validator::make($request->all(),[
+
+            'name' => 'required',
+            'photo' => 'required',
+            'typeTeam' => 'required',
+            'sport' => 'required',
+            'country' => 'required'
+
+        ]);
+
+        if($validator->fails())
+            return $validator->errors()->toJson();
+        return 'ok';
+    }
+
+
+    public function index(){
+
+    }
+    public function update(Request $request, $id){
+
+    }
+    public function destroy($id){
+
+    }
+
 }
