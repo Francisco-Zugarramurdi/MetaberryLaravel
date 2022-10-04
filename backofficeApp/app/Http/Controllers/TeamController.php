d72ee4fe5a3baef84c610b27b7f7137b49b6ba24
nicolasMoraGnd <89613192+nicolasMoraGnd@users.noreply.github.com>
2022-10-04 20:58:42 -0300

    id for team model

--- backofficeApp/app/Http/Controllers/TeamController.php
+++ backofficeApp/app/Http/Controllers/TeamController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Team;
+use App\Models\Sport;
+use App\Models\Country;
 use Illuminate\Support\Facades\Validator;
 
 class TeamController extends Controller
@@ -19,7 +21,7 @@ class TeamController extends Controller
         catch (QueryException $e){
 
             return [
-                "error" => 'Cannot create user',
+                "error" => 'Cannot create Team',
                 "trace" => $e -> getMessage()
             ];
 
@@ -39,18 +41,50 @@ class TeamController extends Controller
 
         if($validator->fails())
             return $validator->errors()->toJson();
+        
+        if(Team::withTrashed()->where('name', $request -> post("name")) -> exists())
+            return 'The team already exist'
+        
+        if(!Sport::withTrashed()->where('name', $request -> post("sport")) -> exists())
+            return 'the sport do not exist'
+
+        if(!Country::withTrashed()->where('name', $request -> post("country")) -> exists())
+            return 'the country do not exist'
+        
         return 'ok';
     }
 
+    private function createTeam(){
+        $user = Team::create([
+            'name' => $request -> post("name"),
+            'typeTeam' => $request -> post("typeTeam"),
+            'photo' => $request -> post("photo"),
+            'id_sports' => Sport::where('name', $request -> post("sport"))->id;
+            'id_country' => Country::where('name', $request -> post("country"))->id;
+        ]);
+    }
 
     public function index(){
-
+        $teams = Team::join('countries','countries.id','=','teams.id_countries')->join('sports','sports.id','=','teams.id_sports')->get();
+        return $teams;
     }
+
     public function update(Request $request, $id){
 
     }
-    public function destroy($id){
 
+    public function destroy($id){
+        try{
+            Team::findOrFail($id)->delete();
+            
+
+        }
+        catch(QueryException $e){
+            return [
+                "error" => 'Cannot delete user',
+                "trace" => $e -> getMessage()
+            ];
+        }
     }
 
 }
--- backofficeApp/app/Models/Team.php
+++ backofficeApp/app/Models/Team.php
@@ -4,8 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Team extends Model
 {
-    use HasFactory;
+    use HasFactory,SoftDeletes;
+    protected $table = "teams";
+    protected $fillable = [ 'id', 'name','photo', 'type_text', 'id_sport', 'id_country'];
 }
