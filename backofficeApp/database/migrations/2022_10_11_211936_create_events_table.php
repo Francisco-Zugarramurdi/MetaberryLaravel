5651c19fef9befc4c2d068087d863af3e73022d3
sofiamotta <sofiamottasanchez03@gmail.com>
2022-10-11 18:36:24 -0300

    Event Backoffice - Setting up dev. env,

---
+++ backofficeApp/database/migrations/2022_10_11_211936_create_events_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->string('details');
+            $table->bigInteger('id_sports')->unsigned();  
+            $table->bigInteger('id_countries')->unsigned();
+            $table->string('relevance');  
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
