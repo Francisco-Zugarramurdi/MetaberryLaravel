5fc9934fbf8022e982b537a815835f61a5b62cc0
sofiamotta <sofiamottasanchez03@gmail.com>
2022-10-29 14:03:43 -0300

    Backoffice Styles Updated

--- backofficeApp/resources/views/auth/login.blade.php
+++ backofficeApp/resources/views/auth/login.blade.php
@@ -1,55 +1,34 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <h1>{{ __('Backoffice') }}</h1>
-                <p>{{ __('Please log in') }}</p>
+<div class="login-container">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+    <div class="form-login-container">
+        <div class="header-container">
+            <h1>Backoffice</h1>
+            <p>Please log in</p>
+        </div>
+        <div class="form-container">
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
+                <div class="form-up-container">
+                    
+                    <label>
+                        Rol
+                        <input id="type" type="text" name="type" value="{{ old('type') }}" autofocus>
+                    </label>
 
-                        <div class="row mb-3">
-                            <label for="type" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
+                    <label>
+                        Password
+                        <input id="password" type="password" name="password" required>
+                    </label>
 
-                            <div class="col-md-6">
-                                <input id="type" type="text" class="form-control @error('type') is-invalid @enderror" name="type" value="{{ old('type') }}" required autocomplete="type" autofocus>
-
-                                @error('type')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
                 </div>
-            </div>
+                <div class="form-down-container">
+                    <button type="submit">Login</button>
+                </div>
+                
+            </form>
         </div>
     </div>
 </div>
