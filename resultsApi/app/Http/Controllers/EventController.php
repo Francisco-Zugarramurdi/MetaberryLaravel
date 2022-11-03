aa5d8cd65b451aa7b8f88960b8f5641fe689f461
sofiamotta <sofiamottasanchez03@gmail.com>
2022-11-02 21:13:24 -0300

    Nada

--- resultsApi/app/Http/Controllers/EventController.php
+++ resultsApi/app/Http/Controllers/EventController.php
@@ -8,38 +8,71 @@ use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\DB;
 use App\Models\Event;
 use App\Models\Result;
+use App\Models\Team;
 
 class EventController extends Controller
 {
-    public function Index($id){
-        //validar
+   
+    /*
+        1. Index en Landing Page 
+            Filtrar por: relevancia
 
-        try{
-            return $this->handleIndex($id);        
-        }
-        catch(QueryException $e){
-            return $e;
-        }
-    
-    }
-    private function handleIndex($id){
-        $event = Event::findOrFail($id);
-        $result = Result::where('id_events',$id);
+            (   ) (   ) (   )
+            (   ) (   ) (   )
+            (   ) (   ) (   )
+
+            TODOS LOS EVENTOS + RESULTADOS
+
+        2. Index en Scores
+            -> Filtrar por: fecha, deporte, pais.
+
+
+        3. Index en Evento
+        4. Index en Feed Usuario
+            -> Filtrar por: equipos favoritos
+        5. Index en Perfil Equipo
+            -> Filtrar por: evento relevante al equipo
+            
         
-    }
-    private function handleResults($result){
-        if($result->type_results == 'points_sets'){
-            $this->handleResultSet();
-        }
-        if($result->type_results == 'results_points'){
-            $this->handleResultSet();
-        }
-        if($result->type_results == 'results_upward'){
-            $this->handleResultSet();
-        }
-        if($result->type_results == 'results_downward'){
-            $this->handleResultSet();
-        }
+        $eventBySet = DB::table('AllEvents')
+        ->join('points_sets', 'AllEvents.resultId', 'points_sets.id_results')->get();
+
+        $eventByPoint = DB::table('AllEvents')
+        ->join('results_points', 'AllEvents.resultId', 'results_points.id_results')->get();
+
+        $eventByMarkUp = DB::table('AllEvents')
+        ->join('results_points', 'AllEvents.resultId', 'results_points.id_results')->get();
+
+        $eventByMarkDown = DB::table('AllEvents')
+        ->join('results_points', 'AllEvents.resultId', 'results_points.id_results')->get();
+
+
+        array[] lindo
+
+        foreach($eventBySet as set)
+
+            -> array lindo
+        
+    */
+
+    public function IndexCards(Request $request){
+
+        // $mergedCollection = $events->toBase()->merge($team);
+
+        // dd($mergedCollection);
+        // //validate
+
+        $events = DB::table('AllEvents')->get();
+        $points = DB::table('AllEvents')->join('results_points', 'AllEvents.resultId', 'results_points.id_results')->get();
+        $sets = DB::table('AllEvents')->join('points_sets', 'AllEvents.resultId', 'points_sets.id_results')->get();
+        $markUp = DB::table('AllEvents')->join('results_upward', 'AllEvents.resultId', 'results_upward.id_results')->get();
+        $markDown = DB::table('AllEvents')->join('results_downward', 'AllEvents.resultId', 'results_downward.id_results')->get();
+
+
+        $indexEvent = $events->toBase()->merge([$points, $sets, $markUp, $markDown]);
+
+        dd($indexEvent);
+
     }
 
 }
--- resultsApi/routes/web.php
+++ resultsApi/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\EventController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/pepe', [EventController::class, 'IndexCards']);
