16fd24c81ae8a842bc3682ecf5893ab0c85d34c8
LucasMedina16 <48605639+LucasMedina16@users.noreply.github.com>
2022-11-07 15:32:19 -0300

    SQL Changes + SQL Data complete

--- datos.sql
+++ datos.sql
@@ -1,10 +1,28 @@
-insert into sports(name, icon) values ('Futbol','sports_soccer'), 
+insert into sports(name, icon) values 
+('Futbol','sports_soccer'), 
 ('Tenis','sports_tennis'), 
-('Ping Pong','sports_handball'); 
-insert into countries(name, photo) values ('Uruguay','default_img_do_not_delete.jpg'), 
+('Ping Pong','sports_handball'),
+('Carreras','sports_motorsports'), 
+('Piscina','pool'), 
+('Basket','sports_basketball'), 
+('Gym','sports_gymnastics'), 
+('Surf','surfing'), 
+('Golf','sports_golf'), 
+('Volley','sports_volleyball'), 
+('MMA','sports_martial_arts'), 
+('Rugby','sports_rugby'), 
+('Boxeo','sports_mma'), 
+('Patin','roller_skating');
+insert into countries(name, photo) values 
+('Uruguay','default_img_do_not_delete.jpg'), 
 ('Argentina','default_img_do_not_delete.jpg'), 
-('Brasil','default_img_do_not_delete.jpg'); 
-insert into players(name, surname, photo) values ('Luis','Suarez','default_img_do_not_delete.jpg'), 
+('Brasil','default_img_do_not_delete.jpg'),
+('Estados Unidos','default_img_do_not_delete.jpg'), 
+('Chile','default_img_do_not_delete.jpg'), 
+('Alemania','default_img_do_not_delete.jpg'), 
+('España','default_img_do_not_delete.jpg');
+insert into players(name, surname, photo) values 
+('Luis','Suarez','default_img_do_not_delete.jpg'), 
 ('Luis','Perez','default_img_do_not_delete.jpg'), 
 ('Martin','Rodriguez','default_img_do_not_delete.jpg'), 
 ('Joaquín','Smith','default_img_do_not_delete.jpg'), 
@@ -47,15 +65,28 @@ insert into players(name, surname, photo) values ('Luis','Suarez','default_img_d
 ('Viviana','Sosa','default_img_do_not_delete.jpg'), 
 ('Mario','McArthur','default_img_do_not_delete.jpg'); 
 insert into events(name, details, id_sports, id_countries, date,  relevance) values 
-('Gran parque central, team1 vs team2','Se juega en el parque  central',1,1,"2022-01-01",5), 
-('Campeon del siglo, team2 vs team2','Se juega en la  bombonera',1,2,"2022-02-01",4);
-insert into teams(name, type_teams, id_sports, id_countries, photo) values ('Nacional', 'grupal',1,1,'default_img_do_not_delete.jpg'), 
-('Boca', 'grupal',1,2,'default_img_do_not_delete.jpg'), 
-('Peñarol', 'grupal',1,1,'default_img_do_not_delete.jpg'), 
-('Racing', 'grupal',1,2,'default_img_do_not_delete.jpg'), 
-('PingPong team1', 'grupal',3,3,'default_img_do_not_delete.jpg'), 
-('PingPong team2', 'grupal',3,2,'default_img_do_not_delete.jpg'); 
-insert into players_teams(id_players, id_teams, contract_start, contract_end, status) values (1,1,"2020-01-01","2022-12-12","Active"), 
+('Gran parque central','Se juega en el parque  central',1,1,"2022-01-01",5),
+('Campeon del siglo','Se juega en la  bombonera',1,2,"2022-02-01",4),
+('Partido de Tennis','Se juega en el club nautico',2,1,"2022-01-01",5), 
+('Carrera de autos','Se corre en Nascar',4,6,"2022-01-01",5), 
+('Carrera de personas','Se corre en el hipodromo',4,5,"2022-01-01",5); 
+
+insert into teams(name, type_teams, id_sports, id_countries, photo) values 
+('Nacional', 'Group',1,1,'default_img_do_not_delete.jpg'), 
+('Boca', 'Group',1,2,'default_img_do_not_delete.jpg'), 
+('Peñarol', 'Group',1,1,'default_img_do_not_delete.jpg'), 
+('Racing', 'Group',1,2,'default_img_do_not_delete.jpg'), 
+('Tennis team1', 'Individual',2,2,'default_img_do_not_delete.jpg'), 
+('Tennis team2', 'Individual',2,2,'default_img_do_not_delete.jpg'),
+('Auto1', 'Individual',4,6,'default_img_do_not_delete.jpg'), 
+('Auto2', 'Individual',4,6,'default_img_do_not_delete.jpg'), 
+('Auto3', 'Individual',4,6,'default_img_do_not_delete.jpg'), 
+('Auto4', 'Individual',4,6,'default_img_do_not_delete.jpg'), 
+('Corredor1', 'Individual',4,5,'default_img_do_not_delete.jpg'), 
+('Corredor2', 'Individual',4,5,'default_img_do_not_delete.jpg');
+
+insert into players_teams(id_players, id_teams, contract_start, contract_end, status) values 
+(1,1,"2020-01-01","2022-12-12","Active"), 
 (2,1,"2020-01-01","2022-12-12","Active"), 
 (3,1,"2020-01-01","2022-12-12","Active"), 
 (4,1,"2020-01-01","2022-12-12","Active"), 
@@ -67,35 +98,44 @@ insert into players_teams(id_players, id_teams, contract_start, contract_end, st
 (10,1,"2020-01-01","2022-12-12","Active"), 
 (11,2,"2020-01-01","2022-12-12","Active"), 
 (12,2,"2020-01-01","2022-12-12","Active"), 
-(13,2,"2020-01-01","2022-12-12","Active"), 
-(14,2,"2020-01-01","2022-12-12","Active"),
-(15,2,"2020-01-01","2022-12-12","Active"), 
-(16,2,"2020-01-01","2022-12-12","Active"), 
-(17,2,"2020-01-01","2022-12-12","Active"), 
-(18,2,"2020-01-01","2022-12-12","Active"), 
-(19,2,"2020-01-01","2022-12-12","Active"), 
-(20,2,"2020-01-01","2022-12-12","Active"), 
-(21,3,"2020-01-01","2022-12-12","Active"), 
-(22,3,"2020-01-01","2022-12-12","Active"), 
-(23,3,"2020-01-01","2022-12-12","Active"), 
-(24,3,"2020-01-01","2022-12-12","Active"), 
-(25,3,"2020-01-01","2022-12-12","Active"), 
-(26,4,"2020-01-01","2022-12-12","Active"), 
-(27,4,"2020-01-01","2022-12-12","Active"), 
-(28,4,"2020-01-01","2022-12-12","Active"), 
-(29,4,"2020-01-01","2022-12-12","Active"), 
-(30,4,"2020-01-01","2022-12-12","Active"), 
-(31,5,"2020-01-01","2022-12-12","Active"), 
-(32,6,"2020-01-01","2022-12-12","Active"); 
-insert into extras(name, surname, rol, photo) values ('Sandro','Brown','Otro','default_img_do_not_delete.jpg'), 
+(13,2,"2020-11-01","2022-12-12","Active"), 
+(14,2,"2020-11-01","2022-12-12","Active"),
+(15,2,"2020-11-01","2022-12-12","Active"), 
+(16,2,"2020-11-01","2022-12-12","Active"), 
+(17,2,"2020-12-01","2022-12-12","Active"), 
+(18,2,"2020-10-01","2022-12-12","Active"), 
+(19,2,"2020-10-01","2022-12-12","Active"), 
+(20,2,"2020-12-01","2022-12-12","Active"), 
+(21,3,"2020-11-01","2022-12-12","Active"), 
+(22,3,"2020-11-01","2022-12-12","Active"), 
+(23,3,"2020-04-01","2022-12-12","Active"), 
+(24,3,"2020-05-01","2022-12-12","Active"), 
+(25,3,"2020-06-01","2022-12-12","Active"), 
+(26,4,"2020-07-01","2022-12-12","Active"), 
+(27,4,"2020-08-01","2022-12-12","Active"), 
+(28,4,"2020-09-01","2022-12-12","Active"), 
+(29,4,"2020-07-01","2022-12-12","Active"), 
+(30,4,"2020-08-01","2022-12-12","Active"), 
+(31,5,"2020-09-01","2022-12-12","Active"), 
+(32,6,"2020-11-01","2022-12-12","Active"),
+(39,5,"2020-01-01","2022-12-12","Active"), 
+(40,6,"2020-03-01","2022-12-12","Active"),
+(33,7,"2020-05-01","2022-12-12","Active"),
+(34,8,"2020-06-01","2022-12-12","Active"),
+(35,9,"2020-05-01","2022-12-12","Active"),
+(36,10,"2020-01-11","2022-12-12","Active"),
+(37,11,"2020-11-01","2022-12-12","Active"),
+(38,12,"2020-03-01","2022-12-12","Active");
+insert into extras(name, surname, rol, photo) values 
+('Sandro','Brown','Otro','default_img_do_not_delete.jpg'), 
 ('John','Mayer','Director técnico','default_img_do_not_delete.jpg'), 
 ('Walter','Diaz','Director técnico','default_img_do_not_delete.jpg'), 
 ('John','Williams','Coach','default_img_do_not_delete.jpg'), 
 ('Walter','Diaz','Preparador Fisico','default_img_do_not_delete.jpg'), 
 ('Walter','Blanco','Coach','default_img_do_not_delete.jpg'), 
-('Jesus','Ruiz','Coach','default_img_do_not_delete.jpg'), 
-('Ken','Ruiz','Preparador Fisico','default_img_do_not_delete.jpg');
-insert into extra_compose(id_extra, id_teams, contract_start, contract_end) values (1,1,"2022-02-01","2022-12-12"), 
+('Jesus','Ruiz','Coach','default_img_do_not_delete.jpg');
+insert into extra_compose(id_extra, id_teams, contract_start, contract_end) values 
+(1,1,"2022-02-01","2022-12-12"), 
 (2,1,"2022-02-01","2022-12-12"), 
 (5,1,"2022-02-01","2022-12-12"), 
 (3,2,"2022-02-01","2022-12-12"), 
@@ -106,28 +146,47 @@ insert into events_teams(id_teams, id_events) values
 (1,1), 
 (2,1), 
 (3,2), 
-(4,2); 
+(4,2), 
+(5,3), 
+(6,3), 
+(7,4), 
+(8,4), 
+(9,4), 
+(10,4), 
+(11,5), 
+(12,5);
 insert into leagues(name, details, photo) values 
 ('Copa Libertadores','La Copa Libertadores de América, denominada  oficialmente Copa Conmebol Libertadores, es un torneo anual  internacional oficial de fútbol organizado por la Confederación  Sudamericana de Fútbol, creado en 1960 bajo la denominación de Copa de  Campeones de América o Copa Campeones de América','default_img_do_not_delete.jpg'), 
-('Liga PongPingPam', 'Se juega al ping pong re piola','default_img_do_not_delete.jpg'); 
-insert into leagues_events(id_events, id_leagues) values (1,1),
-(2,1); 
-insert into leagues_countries(id_leagues, id_countries) values (1,1), 
-(2,3); 
+('Liga Tennis', 'Se juega al Tennis','default_img_do_not_delete.jpg'),
+('Nascar', 'Fium','default_img_do_not_delete.jpg'); 
+insert into leagues_events(id_events, id_leagues) values 
+(1,1),
+(2,1),
+(3,2),
+(4,3);
+insert into leagues_countries(id_leagues, id_countries) values 
+(1,1), 
+(2,3),
+(3,6);
 insert into sanctions(id_events, sancion) values 
 (1,'Tarjeta Amarilla'), 
 (1,'Tarjeta Roja'), 
 (2,'Tarjeta Amarilla'), 
 (2,'Tarjeta Roja'); 
-insert into sanctions_players(id_sancion, id_players, minute) values (1,1,10),
+insert into sanctions_players(id_sancion, id_players, minute) values 
+(1,1,10),
 (2,2,10), 
 (3,12,3), 
 (4,14,4);
 insert into referee(name, surname, photo) values 
 ('Pedro', 'PicaPiedra','default_img_do_not_delete.jpg'), 
 ('Hernesto', 'Perez','default_img_do_not_delete.jpg'); 
-insert into referee_events(id_referee, id_events, dates) values (1,1,"2022-01-01"), 
-(2,2,"2022-01-01"); 
+insert into referee_events(id_referee, id_events, dates) values 
+(1,1,"2022-01-01"), 
+(2,2,"2022-01-01"),
+(1,3,"2022-01-01"),
+(2,4,"2022-01-01"),
+(2,5,"2022-01-01");
 insert into users_data(name,points,type_of_user,total_points, photo, credit_card)  values 
 ('Lukitas',10000,'paid_yearly',240000,'default_img_do_not_delete.jpg','4321432143214321'), 
 ('Yves',3000,'paid_monthly',20000,'default_img_do_not_delete.jpg','4321432143214321'), 
@@ -139,16 +198,8 @@ insert into users(id, name, email,password) values
 (3,'Zara zugarramurdi','zarita@gmail.com','HASH01Zara'), (4,'Fede  
 Notienesurname','elfedefacha@gmail.com','Fedenotienehasheadalacontra'),
 (5,'Nicolas Mora','nico@gmail.com','HASH01Nico'); 
-insert into post(post, id_users_data,dates,id_events,number_of_likes) values ('Andrew Tate idolo por siempre',1,"2022-01-01",1,1000), 
-('Andrew es un pelado facha',4,"2022-01-01",1,1000), 
-('Que lindos colores los de las camisetas!',2,"2022-01-01",2,1000);
-insert into user_likes(id_post, id_users_data) values (1,1), 
-(1,2), 
-(1,3), 
-(1,4), 
-(2,1), 
-(3,1); 
-insert into premium_league(id_users_data, id_leagues) values (1,1), 
+insert into premium_league(id_users_data, id_leagues) values 
+(1,1), 
 (1,2), 
 (2,2), 
 (3,1), 
@@ -156,7 +207,8 @@ insert into premium_league(id_users_data, id_leagues) values (1,1),
 (4,1), 
 (5,1), 
 (5,2); 
-insert into premium_sports(id_users_data, id_sports) values (1,1), 
+insert into premium_sports(id_users_data, id_sports) values 
+(1,1), 
 (1,3), 
 (2,2), 
 (3,3), 
@@ -164,12 +216,14 @@ insert into premium_sports(id_users_data, id_sports) values (1,1),
 (4,1), 
 (5,3), 
 (5,2); 
-insert into premium_events(id_users_data, id_events) values (1,1), 
+insert into premium_events(id_users_data, id_events) values 
+(1,1), 
 (2,2), 
 (3,2),
 (4,1),  
 (5,2); 
-insert into premium_teams(id_users_data, id_teams) values (1,1), 
+insert into premium_teams(id_users_data, id_teams) values 
+(1,1), 
 (1,2), 
 (1,5), 
 (2,4), 
@@ -179,22 +233,41 @@ insert into premium_teams(id_users_data, id_teams) values (1,1),
 (4,1), 
 (5,4), 
 (5,5); 
-insert into favourite_user(id_users_data, id_teams) values (1,1), 
+insert into favourite_user(id_users_data, id_teams) values 
+(1,1), 
 (2,2), 
 (3,4), 
 (1,5), 
 (5,6); 
+
 insert into results(results, id_events,type_results) values 
 ('Ganador Boca',1,'results_points'), 
-('Ganador Racing',2,'results_points');
-insert into bets(amount, id_users_data, id_events, id_teams, dates)  values 
-(240000,1,1,1,"2022-01-01"), 
-(1000,2,1,1,"2022-01-01"), 
-(42000,3,1,1,"2022-01-01"), 
-(200,5,1,1,"2022-01-01"), 
-(100,2,2,4,"2022-01-01"), 
-(10230,3,2,4,"2022-01-01"), 
-(1120,4,2,4,"2022-01-01");
+('Ganador Racing',2,'results_points'),
+('Ganador Tennis1',3,'points_sets'),
+('Ganador Auto3',4,'results_downward'),
+('Ganador Corredor2',5,'results_upward');
+
+insert into points_sets(number_set, points_set, id_teams, id_results) values
+(1,5,5,3),
+(1,8,6,3),
+(2,5,5,3),
+(2,6,6,3);
+
+insert into results_points(id_results, id_teams, point, id_players) values
+(1,1,2,1),
+(1,2,5,15),
+(2,3,1,21),
+(2,4,4,26);
+
+insert into results_upward(id_results,id_teams,position,result) values
+(5,11,2,10),
+(5,12,1,12);
+
+insert into results_downward(id_results,id_teams,position,result) values
+(4,7,3,16),
+(4,8,4,15),
+(4,9,1,10),
+(4,10,2,9);
 
 insert into ads (view_counter, views_hired, url, image, size) values
 (1,10,'url.url','sphoto.com','small'),
@@ -206,7 +279,8 @@ insert into ads (view_counter, views_hired, url, image, size) values
 insert into tags(tag) values
 ('Futbol'),
 ('Tenis'),
-('PingPong');
+('PingPong'),
+('Carreras');
 
 insert into ad_tags(id_tag, id_ad) values
 (1,1),
