42c338b7c01a2b5c8d15d8c19c5598709f5bc00f
nicolasMoraGnd <89613192+nicolasMoraGnd@users.noreply.github.com>
2022-11-15 23:14:27 -0300

    ads in scores

--- MetaBerryServer.bat
+++ MetaBerryServer.bat
@@ -5,5 +5,6 @@ start Server1.bat
 start Server2.bat
 start Server3.bat
 start Server4.bat
+start Server5.bat
 start chrome.exe "http://127.0.0.1:8005/"
 start chrome.exe "http://127.0.0.1:8009/"
---
+++ Server5.bat
@@ -0,0 +1,7 @@
+@echo OFF
+MODE con: cols=100 lines=30
+@echo OFF
+tictle adsApi
+
+cd adsApi
+php artisan serve --port=8003
